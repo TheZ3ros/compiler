b7db79b00353788e8aa15aaf613f429ace9866ba
TheZ3ros <alepod9806@gmail.com>
2026-08-15 12:27:09 +0200

    1.2: the driver now generates the filename return_2.i

--- return_2 compiler/driver.c
+++ return_2 compiler/driver.c
@@ -1,5 +1,9 @@
+//1.1
 #include <unistd.h>
 #include <stdio.h>
+//1.2
+#define _GNU_SOURCE 
+#include <string.h> //necessario per strlen()
 
 int main(int argc, char *argv[]){
     int opt;
@@ -25,5 +29,13 @@ int main(int argc, char *argv[]){
     }
 
     printf("File da processare: %s\n", argv[optind]);
+    //fase 1.2: devo produrre return_2.i, pertanto tronco il .c, ovvero gli ultimi 2 caratteri del file di input e li sostituisco con .i tramite asprintf()
+    char *input_file = argv[optind];
+    char *output_file = NULL;
+
+    int len_truncated = strlen(input_file) -2;
+    asprintf(&output_file, "%.*s.i", len_truncated, input_file);
+    printf("File prodotto: %s\n", output_file);
+
     return 0;
 }
